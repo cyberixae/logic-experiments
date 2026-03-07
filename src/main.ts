9c09be6de571555f463af31b80b924cb2fee48ef
Toni Ruottu <toni.ruottu@iki.fi>
2026-03-07 23:23:43 +0200

    prompt

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import * as readline from 'readline'
-import { repl } from './repl'
+import { repl } from './interactive/repl'
 import { example } from './theorems/example'
 
 const main = () => {
   const rl = readline.createInterface({ input: process.stdin })
   const gen = repl(example.goal)
   process.stdout.write(gen.next('').value ?? '')
+  process.stdout.write('\n> ')
   rl.on('line', (line) => {
     const { value, done } = gen.next(line.trim())
     if (value) process.stdout.write(value)
     if (done) rl.close()
+    process.stdout.write('\n> ')
   })
   rl.on('SIGINT', () => {
     rl.close()
