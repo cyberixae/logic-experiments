64d475a3460cdf6fd52a29452d33bdc05aec5f4d
Toni Ruottu <toni.ruottu@iki.fi>
2026-03-09 21:16:45 +0200

    syaani-paprika-kettu

--- src/theorems/index.ts
+++ src/theorems/index.ts
@@ -1,9 +1,11 @@
 import { harmaaPuolukkaTiikeri } from './harmaa-puolukka-tiikeri'
 import { violettiLuumuBiisoni } from './violetti-luumu-biisoni'
+import { syaaniPaprikaKettu } from './syaani-paprika-kettu'
 
 export const theorems = {
-  harmaaPuolukkaTiikeri ,
-  violettiLuumuBiisoni ,
+  harmaaPuolukkaTiikeri,
+  violettiLuumuBiisoni,
+  syaaniPaprikaKettu,
 }
 export type Theorems = typeof theorems
 export const isTheoremKey = (k: string): k is keyof Theorems => k in theorems
