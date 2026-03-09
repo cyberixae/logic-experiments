b1bc7048acee7463db2e2f880a71838e99057d46
Toni Ruottu <toni.ruottu@iki.fi>
2026-03-09 18:45:33 +0200

    namegen

---
+++ scripts/name-gen.ts
@@ -0,0 +1,68 @@
+
+const colors = [
+  'harmaa',
+  'keltainen',
+  'oranssi',
+  'punainen',
+  'ruskea',
+  'sininen',
+  'syaani',
+  'turkoosi',
+  'violetti',
+]
+const greens = [
+  'ananas',
+  'aprikoosi',
+  'banaani',
+  'fenkoli',
+  'karpalo',
+  'kirsikka',
+  'kookos',
+  'kurkku',
+  'luumu',
+  'mango',
+  'munakoiso',
+  'mustikka',
+  'nektariini',
+  'oliivi',
+  'omena',
+  'paprika',
+  'persikka',
+  'peruna',
+  'pomelo',
+  'porkkana',
+  'puolukka',
+  'retiisi',
+  'selleri',
+  'sipuli',
+  'sitruuna',
+  'tomaatti',
+]
+const animals = [
+  'ahma',
+  'alpakka',
+  'biisoni',
+  'hirvi',
+  'kameli',
+  'kauris',
+  'kenguru',
+  'kettu',
+  'kirahvi',
+  'koala',
+  'leijona',
+  'lepakko',
+  'okapi',
+  'oranki',
+  'panda',
+  'pantteri',
+  'puuma',
+  'seepra',
+  'tapiiri',
+  'tiikeri',
+  'virtahepo',
+]
+
+const pick = (arr) => arr[Math.floor(Math.random() * arr.length)]
+const gen = () => [colors, greens, animals].map(pick).join('-')
+
+console.log(Array(10).fill(0).map(() => gen()).join('\n'))
